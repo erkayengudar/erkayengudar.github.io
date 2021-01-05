329b4b3b45d26969a4c42aa05d96173e8fba0436
Kaan Kuscu <33625775+ksckaan1@users.noreply.github.com>
2021-01-05 23:40:53 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,1 +1,32 @@
 //import * as $ from "jquery";
+
+var person1 = $("#person1")
+var person2 = $("#person2")
+var person3 = $("#person3")
+
+person1.hover(function(){
+    person2.css("filter","blur(8px)")
+    person3.css("filter","blur(8px)")
+})
+person1.mouseout(function(){
+    person2.css("filter","blur(0px)")
+    person3.css("filter","blur(0px)")
+})
+
+person2.hover(function(){
+    person1.css("filter","blur(8px)")
+    person3.css("filter","blur(8px)")
+})
+person2.mouseout(function(){
+    person1.css("filter","blur(0px)")
+    person3.css("filter","blur(0px)")
+})
+
+person3.hover(function(){
+    person2.css("filter","blur(8px)")
+    person1.css("filter","blur(8px)")
+})
+person3.mouseout(function(){
+    person2.css("filter","blur(0px)")
+    person1.css("filter","blur(0px)")
+})
