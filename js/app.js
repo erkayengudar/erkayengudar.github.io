8131a92ce0bf5ab84f8c4906ad6edd83c821458b
Kaan Kuscu <33625775+ksckaan1@users.noreply.github.com>
2021-01-08 15:03:17 +0300

    update general design

--- js/app.js
+++ js/app.js
@@ -1,6 +1,66 @@
 // import * as $ from "jquery";
 // import "TimelineLite.min.js";
 
+
+//when page is scrolled, topmenu will be fixed
+
+window.onscroll = function() {scrollTopMenu()};
+
+const topMenuEffect = new TimelineLite({
+    paused: true
+});
+
+topMenuEffect.to("#topMenu", 0, {
+    position: 'fixed',
+    ease: Power2.easeOut
+})
+.to("#topMenu", 0, {
+    boxShadow: '0px 0px 25px black',
+    ease: Power2.easeOut
+})
+.to(".topBanner", 0, {
+    paddingTop: '60px',
+    ease: Power2.easeOut
+})
+
+function scrollTopMenu() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    topMenuEffect.play()
+  } else {
+    topMenuEffect.reverse()
+  }
+}
+
+
+//Mobile menu effects
+const menuEffect = new TimelineLite({
+    paused: true
+});
+
+menuEffect.to("#topMenu", 0.5, {
+    height: '100vh',
+    ease: Power2.easeOut
+})
+var isMenuActive = false
+
+function Menu(){
+    if (isMenuActive){
+        isMenuActive= false
+        menuEffect.reverse()
+    }else{
+        isMenuActive= true
+        menuEffect.play()
+    }
+}
+$(".menuIcon").click(function (e) {
+    Menu()
+})
+$(".mobileMenuList a").click(function (e) {
+    Menu()
+})
+
+
+//who are we? effects
 var person1 = $("#person1")
 var person2 = $("#person2")
 var person3 = $("#person3")
@@ -32,28 +92,3 @@ person3.mouseout(function () {
     person1.css("filter", "blur(0px)")
 })
 
-const menuEffect = new TimelineLite({
-    paused: true
-});
-
-menuEffect.to("#topMenu", 0.5, {
-    height: '100vh',
-    ease: Power2.easeOut
-})
-var isMenuActive = false
-
-function Menu(){
-    if (isMenuActive){
-        isMenuActive= false
-        menuEffect.reverse()
-    }else{
-        isMenuActive= true
-        menuEffect.play()
-    }
-}
-$(".menuIcon").click(function (e) {
-    Menu()
-})
-$(".mobileMenuList a").click(function (e) {
-    Menu()
-})
