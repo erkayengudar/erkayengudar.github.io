885605f4bc2c54a4991b474c5f7eb415632e3d92
Kaan Kuscu <33625775+ksckaan1@users.noreply.github.com>
2021-01-09 19:02:07 +0300

    hanimelilokma design update

--- hanimeli/js/app.js
+++ hanimeli/js/app.js
@@ -26,3 +26,38 @@ var phone = document.querySelector(".phone")
 phone.addEventListener('click', function () {
     location.replace("tel:905425396500")
 })
+
+
+//Mobile menu effect
+
+var menuIcon = document.querySelector(".menuIcon")
+var menuLinks = document.querySelector(".mobileMenu a")
+
+const menuEffects = new TimelineLite({
+    paused: true
+})
+
+
+menuEffects.to(".mobileMenu", 0, {
+    display: 'flex'
+})
+.to(".mobileMenu", 0.5, {
+    right: '20px',
+    left: '20px',
+    ease: Power2.easeOut
+})
+var menuOn = false
+menuIcon.addEventListener('click', function () {
+
+    if (menuOn) {
+        menuOn = false
+        menuEffects.reverse()
+    } else {
+        menuOn = true
+        menuEffects.play()
+    }
+})
+menuLinks.addEventListener('click', function () {
+    menuEffects.reverse()
+    menuOn = false
+})
